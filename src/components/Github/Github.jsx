838ea2ef1da835a336c79da5edf38ac544053caa
saurabh-r1 <sidlock2@gmail.com>
2023-09-20 07:43:53 +0530

    optimization in react router

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,16 +1,19 @@
-import { data } from "autoprefixer";
-import React, { useEffect, useState } from "react";
+
+// import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
 
 function Github() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://api.github.com/users/saurabh-r1")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      });
-  }, []);
+    const data = useLoaderData()
+//   const [data, setData] = useState([]);
+//   useEffect(() => {
+//     fetch("https://api.github.com/users/saurabh-r1")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log(data);
+//         setData(data);
+//       });
+//   }, []);
 
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl ">
@@ -19,5 +22,11 @@ function Github() {
     </div>
   );
 }
-
 export default Github;
+
+
+export const githubInfoLoader = async() =>{
+    const response = await fetch ("https://api.github.com/users/saurabh-r1")
+  return response.json ()
+}
+
