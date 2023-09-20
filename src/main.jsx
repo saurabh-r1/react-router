fb7e4436522c14e2a42609fc2fc4580b5d393896
saurabh-r1 <sidlock2@gmail.com>
2023-09-20 07:21:45 +0530

    taking dynamic data in routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import Layout from "./Layout";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
+import User from "./components/User/User";
+import Github from "./components/Github/Github";
 
 // const router = createBrowserRouter([
 //   {
@@ -34,6 +36,8 @@ const router = createBrowserRouter(
       <Route path="" element= {<Home />} />
       <Route path="about" element= {<About/>} />
       <Route path="contact" element= {<Contact />} />
+      <Route path="user/:userid" element= {<User />} />
+      <Route path="github" element= {<Github />} />
     </Route>
   )
 )
